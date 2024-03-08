fb772f64aa39b7710ed732633b796ebf3e30c910
Md Sahil Achanoor <117533136+Mahmadsahil@users.noreply.github.com>
2024-03-08 11:20:47 -0800

    Add files via upload

---
+++ Auto image slider/script.js
@@ -0,0 +1,23 @@
+const images = document.querySelector('.images');
+const points = document.querySelectorAll('.point');
+
+
+const data = [
+    ['https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'],
+    ['https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'],
+    ['https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80'],
+    ['https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80'],
+    ['https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'],
+]
+
+let count = 0;
+window.addEventListener('DOMContentLoaded', () => {
+    setInterval(() => {
+        count++;
+        count > 4 ? count = 0 :
+            images.src = data[count];
+        points.forEach(point => point.classList.remove('active'));
+        points[count].classList.add('active');
+    }, 2000)
+
+})
